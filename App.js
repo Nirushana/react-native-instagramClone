e9fefca7d01ca78d8f7863d8f9da9f2021b06328
Nirushana <kavinda.n.t@gmail.com>
2021-02-03 19:56:42 +0530

    insta clone

--- App.js
+++ App.js
@@ -1,21 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import LandingSrc from './components/auth/LandingSrc';
+import RegisterSrc from './components/auth/RegisterSrc';
+import MainSrc from './components/Main'
+import * as firebase from 'firebase';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './redux/reducers';
+import thunk from 'redux-thunk';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const store = createStore(rootReducer, applyMiddleware(thunk))
+ 
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCbeYN3En03FuInHBTmjXXFVnF__4apjP0",
+  authDomain: "instagramclone-2af7d.firebaseapp.com",
+  projectId: "instagramclone-2af7d",
+  storageBucket: "instagramclone-2af7d.appspot.com",
+  messagingSenderId: "984552466795",
+  appId: "1:984552466795:web:4382f1bdbd0362e0a9442e",
+  measurementId: "G-JC0MR4M5CN"
+};
+
+if(firebase.apps.length == 0) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+export class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state ={
+      loaded: false,
+    }
+  }
+
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user){
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      }else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+      
+    })
+  }
+
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if(!loaded){
+      return(
+        <View>
+          <Text style={{ flex: 1, justifyContent: 'center' }}>Loading</Text>
+        </View>
+      )
+    }
+    if(!loggedIn){
+      return (
+        <NavigationContainer>
+        <Stack.Navigator initialRouteName="Landing">
+          <Stack.Screen name="Landing" component={ LandingSrc } options={{headerShown: false}} />
+          <Stack.Screen name="Register" component={ RegisterSrc } />
+        </Stack.Navigator>
+      </NavigationContainer>
+      );
+    }
+    return(
+      <Provider store={store}>
+        <MainSrc />
+      </Provider>
+      
+    );
+  }
+}
+
+export default App
